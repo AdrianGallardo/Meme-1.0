da34b1a814128aaca5b7b6ac602b68fb220d1494
Adrian Gallardo <adrian.f.gallardo@gmail.com>
2020-06-03 18:08:19 -0500

    Initial Commit

---
+++ Meme 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme 1.0
+//
+//  Created by Adrian Gallardo on 03/06/20.
+//  Copyright © 2020 adriangallardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
